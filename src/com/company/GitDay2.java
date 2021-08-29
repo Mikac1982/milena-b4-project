62c0439529d6a7f564c7fdcb00631de126870b1c
Mikac1982 <sibalic.milena@gmail.com>
2021-08-29 09:51:05 -0400

    added reverse method

--- src/com/company/GitDay2.java
+++ src/com/company/GitDay2.java
@@ -3,5 +3,10 @@ package com.company;
 public class GitDay2 {
     public static void main(String[] args) {
         System.out.println("Good morning! It's a Git Day 2");
+        System.out.println(revStr("Hello World"));
+    }
+
+    public static String revStr(String str){
+        return new StringBuilder(str).reverse().toString();
     }
 }
